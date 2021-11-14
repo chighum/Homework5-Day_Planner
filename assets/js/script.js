daba450c8bfc3760b08f94f09588e129d2c9b551
Charlie Highum <chighum41@gmail.com>
2021-11-14 14:03:07 -0700

    added a .each() iteration in script.js to compare each time block to the current time and then add past, present, or future premade css class to the input form appropriately

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,22 @@ $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
 // add timeblocks 8 am to 5 pm
 // color code the timeblocks by past, present, future
-// make timeblocks input forms with buttons to save user info to local storage
+// start by using moment to get current time, rounded down to the nearest hour
+var currentTime = parseInt(moment().format("H"));
 
-//
+// create a variable containing all of the div's with class "time-block" in an array
+var timeBlockHour = $(".time-block");
+
+// iterate over the time block array comparing current time to the value of each time blocks id
+// the id's for each time block are numbered 8-17 because moment returns a value 1-24 for current hour
+timeBlockHour.each(function (i) {
+  if (currentTime > parseInt(timeBlockHour[i].id)) {
+    $(this).next().addClass("past");
+  } else if (currentTime === parseInt(timeBlockHour[i].id)) {
+    $(this).next().addClass("present");
+  } else if (currentTime < parseInt(timeBlockHour[i].id)) {
+    $(this).next().addClass("future");
+  }
+});
+
+// make timeblocks input forms with buttons to save user info to local storage
