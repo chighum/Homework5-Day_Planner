6cc8f11b1a980f7c0cf6d0ac5b5e87370410047b
Charlie Highum <chighum41@gmail.com>
2021-11-14 12:18:00 -0700

    added script.js and linked it in index.html. in scrip.js added current date to top of the planner using moment.js. added rows for time 8am-5pm and began formatting into three columns for time, text input, and a save button using premade css classes and bootstrap styling

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// add current date to top of planner
+var today = moment();
+$("#currentDay").text(today.format("MMMM Do, YYYY"));
+
+// add timeblocks 8 am to 5 pm
+// color code the timeblocks by past, present, future
+// make timeblocks input forms with buttons to save user info to local storage
+
+//
